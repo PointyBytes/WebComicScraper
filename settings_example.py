ec751fa4370a1caf2b7da72ef0e6b5d90dd606bb
PointyBytes <pointy.bytes@gmail.com>
2024-05-11 19:24:03 +0200

    corrected a spelling error

--- settings_example.py
+++ settings_example.py
@@ -1,6 +1,6 @@
 # settings.py
 base_url = "https://example.com/images/"
 num_images = 50  # Number of pictures expected.
-output_folder = "./Downloads/"  # Path to were your images will be saved.
+output_folder = "./Downloads/"  # Path to where your images will be saved.
 make_cbz = False  # Set this to False if you don't want to create a CBZ file.
 cbz_filename = "Name of the Comic"  # Write the name of your comic here.
